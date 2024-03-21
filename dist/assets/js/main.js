834e19abab423ceaf3f21c1eebbb82caa6f55902
Ricky Drohan <rdrohanirl@yahoo.ie>
2024-03-21 12:57:57 +0000

    Update main.js

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,2 +1,2 @@
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
-document.getElementById("years-exp").innerHTML = new Date().getFullYear() - 2016;
+document.getElementById("years-exp").innerHTML = (new Date().getFullYear() - 2016);
